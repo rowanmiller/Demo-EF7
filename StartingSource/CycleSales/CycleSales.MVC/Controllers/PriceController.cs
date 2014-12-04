d34368b7b699de21ca3fe09b72818714bb313284
rowanmiller <mail@romiller.com>
2014-12-03 18:56:30 -0800

    Updating Starting source with Beta 1 packages and MVC price conversion

---
+++ StartingSource/CycleSales/CycleSales.MVC/Controllers/PriceController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CycleSales.CycleSalesModel;
+using CycleSales.WarrantyModel;
+using Microsoft.Data.Entity;
+
+namespace CycleSalesPublicSite.Controllers
+{
+    public class PriceController : Controller
+    {
+        private CycleSalesContext catalogDb = new CycleSalesContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Convert(decimal exchangeRate)
+        {
+            var convertor = new PriceService(catalogDb);
+            return View(convertor.CalculateForeignPrices(exchangeRate));
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                catalogDb.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
